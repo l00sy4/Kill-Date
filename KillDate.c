cd9e796b3f3e5850b03f7aadba9aa8b6796ab761
l00sy4 <142809264+l00sy4@users.noreply.github.com>
2024-05-25 10:32:16 +0300

    Update KillDate.c

--- KillDate.c
+++ KillDate.c
@@ -5,17 +5,13 @@
 // Credit to @0xLegacyy for helping me with this function :hug:
 SIZE_T GetUnixTimestamp()
 {   
-    // Start of Unix epoch in ticks
     const SIZE_T UNIX_TIME_START = 0x019DB1DED53E8000;
     
-    // A tick is 100ns
     const SIZE_T TICKS_PER_MILLISECOND = 10000; 
     LARGE_INTEGER Time;
 
-    // Read LowPart time from that memory address
     Time.LowPart = *(DWORD*)(0x7FFE0000 + 0x14);
 
-    // Read High1Part time from that memory address
     Time.HighPart = *(LONG*)(0x7FFE0000 + 0x1c); 
     
     // Calculate the Unix timestamp in milliseconds by subtracting the start of the Unix epoch from the current time(Time.QuadPart)
@@ -33,37 +29,28 @@ void SelfDelete()
     FILE_RENAME_INFO* RenameInfoPointer = nullptr;
     size_t RenameInfoSize = sizeof(FILE_RENAME_INFO) + NewStream.size();
 
-    // Allocate memory for the FILE_RENAME_INFO structure
     RenameInfoPointer = malloc(RenameInfoSize);
     if (RenameInfoPointer != NULL) 
     {
        memset(RenameInfoPointer, 0, RenameInfoSize);
     }
 
-    // Set DeleteFile to true in the FILE_DISPOSITION_INFO to mark the file for deletion
     FileDispositionInfo.DeleteFile = TRUE
 
-    // Set the new name to the :$DATA stream buffer and size
     RenameInfoPointer->FileNameLength = sizeof(NewStream);
     RtlCopyMemory(RenameInfoPointer->FileName, NewStream, sizeof(NewStream));
-
-    // Get current file name
     GetModuleFileNameW(NULL, FilePath.data(), MAX_PATH * 2);
 
-    // Open a handle to the current file and rename the data stream
     FileHandle = CreateFileW(FilePath.data(), DELETE | SYNCHRONIZE, FILE_SHARE_READ, NULL, OPEN_EXISTING, NULL, NULL);
     SetFileInformationByHandle(FileHandle, FileDispositionInfo, &FileDispositionInfo, sizeof(FileDispositionInfo);
     CloseHandle(FileHandle);
 
-    // Refresh the data stream, this will cause the file to delete itself once the handle is closed
     HANDLE NewHandle = CreateFileW(FilePath.data(), DELETE | SYNCHRONIZE, FILE_SHARE_READ, NULL, OPEN_EXISTING, NULL, NULL);
     SetFileInformationByHandle(NewHandle, FileDispositionInfo, &FileDispositionInfo, sizeof(FileDispositionInfo);
     CloseHandle(NewHandle);
 
-    // Clean up!
     free(RenameInfoPointer);
     RenameInfoPointer = NULL;
     
-    // Terminate process
     exit();
 }
